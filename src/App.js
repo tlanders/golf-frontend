64e11c8ed305d311da301be43ec6e6ffc4476a00
tlanders <768713+tlanders@users.noreply.github.com>
2020-08-30 12:29:50 -0500

    added reactstrap and basic nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  Collapse,
+  NavItem,
+  NavbarText,
+    NavLink,
+    NavbarToggler
+} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: true
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(event) {
+    this.setState({isOpen: !this.state.isOpen});
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <Navbar color="light" light expand="md" className={"golf-navbar"}>
+            <NavbarBrand href="/">My Golf Scores</NavbarBrand>
+            <NavbarToggler onClick={this.toggle}/>
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                <NavItem>
+                  <NavLink href="/">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/courses">Courses</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/scores">Scores</NavLink>
+                </NavItem>
+                {/*<UncontrolledDropdown nav inNavbar>*/}
+                {/*  <DropdownToggle nav caret>*/}
+                {/*    Options*/}
+                {/*  </DropdownToggle>*/}
+                {/*  <DropdownMenu right>*/}
+                {/*    <DropdownItem>*/}
+                {/*      Option 1*/}
+                {/*    </DropdownItem>*/}
+                {/*    <DropdownItem>*/}
+                {/*      Option 2*/}
+                {/*    </DropdownItem>*/}
+                {/*    <DropdownItem divider />*/}
+                {/*    <DropdownItem>*/}
+                {/*      Reset*/}
+                {/*    </DropdownItem>*/}
+                {/*  </DropdownMenu>*/}
+                {/*</UncontrolledDropdown>*/}
+              </Nav>
+              {/*<NavbarText>Simple Text</NavbarText>*/}
+            </Collapse>
+          </Navbar>
+        </div>
+    );
+  }
 }
 
 export default App;
