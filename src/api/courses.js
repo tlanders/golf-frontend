86fa8e9192bfc73d2186650bb65b45aaccf249dd
Troy Landers <tlanders@landersconsulting.biz>
2020-09-17 18:33:13 -0500

    axios get courses working

--- src/api/courses.js
+++ src/api/courses.js
@@ -1,1 +1,253 @@
 // courses api
+import Axios from "axios";
+
+const getCourses = () => (Axios.get('http://localhost:3001/courses'));
+
+const courses = [
+    {
+        name: "Eagle Bend",
+        key: "eaglebend",
+        location: "Lawrence, KS",
+        image: "/images/eagle-bend.jpg",
+        tees: [
+            {
+                name: "Men's White",
+                key: "menswhite",
+                rating: 68.9,
+                slope: 119,
+                distance: 5989,
+                par: 72,
+                holes: [
+                    {
+                        number: 1,
+                        distance: 370,
+                        par: 4,
+                        handicap: 5
+                    },
+                    {
+                        number: 2,
+                        distance: 325,
+                        par: 4,
+                        handicap: 16
+                    },
+                    {
+                        number: 3,
+                        distance: 499,
+                        par: 5,
+                        handicap: 6
+                    },
+                    {
+                        number: 4,
+                        distance: 120,
+                        par: 3,
+                        handicap: 18
+                    },
+                    {
+                        number: 5,
+                        distance: 320,
+                        par: 4,
+                        handicap: 12
+                    },
+                    {
+                        number: 6,
+                        distance: 379,
+                        par: 4,
+                        handicap: 2
+                    },
+                    {
+                        number: 7,
+                        distance: 483,
+                        par: 5,
+                        handicap: 8
+                    },
+                    {
+                        number: 8,
+                        distance: 127,
+                        par: 3,
+                        handicap: 14
+                    },
+                    {
+                        number: 9,
+                        distance: 390,
+                        par: 4,
+                        handicap: 4
+                    },
+                    {
+                        number: 10,
+                        distance: 360,
+                        par: 4,
+                        handicap: 10
+                    },
+                    {
+                        number: 11,
+                        distance: 344,
+                        par: 4,
+                        handicap: 15
+                    },
+                    {
+                        number: 12,
+                        distance: 105,
+                        par: 3,
+                        handicap: 17
+                    },
+                    {
+                        number: 13,
+                        distance: 500,
+                        par: 5,
+                        handicap: 11
+                    },
+                    {
+                        number: 14,
+                        distance: 335,
+                        par: 4,
+                        handicap: 3
+                    },
+                    {
+                        number: 15,
+                        distance: 300,
+                        par: 4,
+                        handicap: 13
+                    },
+                    {
+                        number: 16,
+                        distance: 409,
+                        par: 4,
+                        handicap: 1
+                    },
+                    {
+                        number: 17,
+                        distance: 153,
+                        par: 3,
+                        handicap: 9
+                    },
+                    {
+                        number: 18,
+                        distance: 470,
+                        par: 5,
+                        handicap: 7
+                    },
+                ]
+            },
+            {
+                name: "Blue",
+                key: "blue",
+                rating: 70.9,
+                slope: 124,
+                distance: 6542,
+                par: 72,
+                holes: [
+                    {
+                        number: 1,
+                        distance: 397,
+                        par: 4,
+                        handicap: 5
+                    },
+                    {
+                        number: 2,
+                        distance: 344,
+                        par: 4,
+                        handicap: 16
+                    },
+                    {
+                        number: 3,
+                        distance: 545,
+                        par: 5,
+                        handicap: 6
+                    },
+                    {
+                        number: 4,
+                        distance: 140,
+                        par: 3,
+                        handicap: 18
+                    },
+                    {
+                        number: 5,
+                        distance: 368,
+                        par: 4,
+                        handicap: 12
+                    },
+                    {
+                        number: 6,
+                        distance: 439,
+                        par: 4,
+                        handicap: 2
+                    },
+                    {
+                        number: 7,
+                        distance: 540,
+                        par: 5,
+                        handicap: 8
+                    },
+                    {
+                        number: 8,
+                        distance: 147,
+                        par: 3,
+                        handicap: 14
+                    },
+                    {
+                        number: 9,
+                        distance: 410,
+                        par: 4,
+                        handicap: 4
+                    },
+                    {
+                        number: 10,
+                        distance: 396,
+                        par: 4,
+                        handicap: 10
+                    },
+                    {
+                        number: 11,
+                        distance: 366,
+                        par: 4,
+                        handicap: 15
+                    },
+                    {
+                        number: 12,
+                        distance: 139,
+                        par: 3,
+                        handicap: 17
+                    },
+                    {
+                        number: 13,
+                        distance: 527,
+                        par: 5,
+                        handicap: 11
+                    },
+                    {
+                        number: 14,
+                        distance: 368,
+                        par: 4,
+                        handicap: 3
+                    },
+                    {
+                        number: 15,
+                        distance: 320,
+                        par: 4,
+                        handicap: 13
+                    },
+                    {
+                        number: 16,
+                        distance: 430,
+                        par: 4,
+                        handicap: 1
+                    },
+                    {
+                        number: 17,
+                        distance: 175,
+                        par: 3,
+                        handicap: 9
+                    },
+                    {
+                        number: 18,
+                        distance: 491,
+                        par: 5,
+                        handicap: 7
+                    },
+                ]
+            }
+        ]
+    }
+];
+
+export default getCourses;
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,16 +1,55 @@
 import React from "react";
+import getCourses from "../api/courses";
+import Axios from "axios";
 
 class Courses extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            courses: null,
+            isLoading: true
+        };
+
+        console.log(this.state.courses);
     }
 
     render() {
-        return (
-            <div>
-                <h1>Courses</h1>
-            </div>
-        );
+        if(this.state.isLoading) {
+            return (
+                <div>
+                    <h1>Courses</h1>
+                    <p>Loading...</p>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <h1>Courses</h1>
+                    <p>Loaded</p>
+                    {
+                        this.state.courses.map((course) => {
+                            return (
+                                <p>{course.name}</p>
+                            );
+                        })
+                    }
+                </div>
+            );
+        }
+    }
+
+    componentDidMount() {
+        // Axios.get('http://localhost:3001/courses')
+        getCourses()
+            .then(result => {
+                console.log('result=' + result.data);
+                this.setState({
+                    courses: result.data,
+                    isLoading: false
+                });
+                console.log(result.data);
+            })
+            .catch(err => console.log('axios courses get err: ', err));
     }
 }
 
